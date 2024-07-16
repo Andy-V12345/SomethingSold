b14912cb1dcb4987b75ab4aa9922713dd5e0a7bd
josyao <joshuayao04@gmail.com>
2024-07-16 01:47:53 -0700

    deleteItem

---
+++ backend/lambda_functions/deleteItem/index.js
@@ -0,0 +1,41 @@
+const knex = require('knex');
+
+const db = knex({
+    client: 'mysql',
+    connection: {
+        host: "somethingsold-db.cr6o0assuiva.us-east-1.rds.amazonaws.com",
+        user: "admin",
+        password: "somethingsold123",
+        port: "3306",
+        database: "somethingsold"
+    }
+});
+
+exports.handler = async (event) => {
+    const response = {
+        statusCode: 200,
+        body: null,
+    };
+
+    let itemId;
+    try {
+        itemId = JSON.parse(event.body).id;
+    } catch (error) {
+        response.statusCode = 400;
+        response.body = JSON.stringify({ error: 'Invalid JSON format in request body' });
+        return response;
+    }
+
+    try {
+        await db('item')
+            .where({ id: itemId })
+            .del();
+
+        response.body = JSON.stringify({ message: 'Item deleted successfully' });
+    } catch (error) {
+        response.statusCode = 500;
+        response.body = JSON.stringify({ error: error.message });
+    }
+
+    return response;
+};
