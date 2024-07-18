56605ed58272a27b6587ddca4856b0b49bc3090b
Andy Vu <andyvu2026@u.northwestern.edu>
2024-07-17 23:20:15 -0500

    updated update-user function for api gateway

--- backend/lambda_functions/user-crud/update-user/index.js
+++ backend/lambda_functions/user-crud/update-user/index.js
@@ -17,16 +17,32 @@ const db = knex({
 
 export const handler = async (event) => {
     const response = {
-        status: 200,
+        statusCode: '200',
         body: null,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Origin': '*'
+        }
     }
 
+    const postData = JSON.parse(event.body);
+    const user_id = event.pathParameters.id;
+
     try {
-        await db('Users').where({id : event.id}).update(event.userData)
+        await db('Users').where({id : user_id}).update({
+            email: postData.email,
+            phone_number: postData.phone_number,
+            location: postData.location,
+            move_in_date: postData.move_in_date,
+            move_out_date: postData.move_out_date,
+            first_name: postData.first_name,
+            last_name: postData.last_name,
+        })
         response.body = "USER_UPDATED"
     }
     catch(err) {
-        response.status = 409
+        response.statusCode = '409'
         response.body = "EMAIL_TAKEN"
     }
 
